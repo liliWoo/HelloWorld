67734636ff88863ec2fe7e8016e7e149689e7cb4
Administrator <13049430269@163.com>
2017-03-11 16:21:50 +0800

    text

--- heimagirl/src/main/java/com/example/itheima/heimagirl/MainActivity.java
+++ heimagirl/src/main/java/com/example/itheima/heimagirl/MainActivity.java
@@ -2,9 +2,26 @@ package com.example.itheima.heimagirl;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.google.gson.Gson;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import jp.wasabeef.glide.transformations.CropCircleTransformation;
+import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -12,34 +29,196 @@ import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
+    @BindView(R.id.lv_girls)
+    ListView lvGirls;
+
+    private Gson mgson;
+    private GirlAdapter mAdapter;
+    private List<ResultBean.ResultsBean> mlist;
+    //private ListView lvGirls;
+    private boolean isLoading;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        //lvGirls = (ListView) findViewById(R.id.lv_girls);
+
+        //sendSyncRequest();
+        sendAsyncRequest();
+
+        mAdapter = new GirlAdapter();
+        mlist = new ArrayList<ResultBean.ResultsBean>();
+
+        lvGirls.setAdapter(mAdapter);
+
+        lvGirls.setOnScrollListener(new AbsListView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(AbsListView view, int scrollState) {
+
+                if (scrollState == SCROLL_STATE_IDLE) {
+                    if (lvGirls.getLastVisiblePosition() == mlist.size() - 1 && !isLoading) {
+
+                        loadmoreData();
+                    }
+                }
+            }
+
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+
+            }
+        });
+
+
+    }
+
+    private void loadmoreData() {
+        isLoading = true;
 
         OkHttpClient okHttpClient = new OkHttpClient();
         //创建网络请求
-        String url = "http://gank.io/api/data/%E7%A6%8F%E5%88%A9/10/1";
+        int page = mlist.size() / 10 + 1;
+        String url = "http://gank.io/api/data/福利/10/" + page;
         Request request = new Request.Builder().get().url(url).build();
-
-        //同步
-        /*try {
-            Response response = okHttpClient.newCall(request).execute();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }*/
         //异步
-            okHttpClient.newCall(request).enqueue(new Callback() {
-                @Override
-                public void onFailure(okhttp3.Call call, IOException e) {
-                }
+        okHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Toast.makeText(MainActivity.this, "网络超时...", Toast.LENGTH_SHORT).show();
+            }
 
-                @Override
-                public void onResponse(okhttp3.Call call, Response response) throws IOException {
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                mgson = new Gson();
+                String result = response.body().string();
+                ResultBean resultBean = mgson.fromJson(result, ResultBean.class);
+                mlist.addAll(resultBean.getResults());
 
-                }
-            });
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        mAdapter.notifyDataSetChanged();
+                    }
+                });
+            }
+        });
+
+
+        isLoading = false;
     }
 
+    private void sendAsyncRequest() {
+        OkHttpClient okHttpClient = new OkHttpClient();
+        //创建网络请求
+        String url = "http://gank.io/api/data/福利/10/1";
+        Request request = new Request.Builder().get().url(url).build();
+        //异步
+        okHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Toast.makeText(MainActivity.this, "网络超时...", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                mgson = new Gson();
+                String result = response.body().string();
+                ResultBean resultBean = mgson.fromJson(result, ResultBean.class);
+                mlist.addAll(resultBean.getResults());
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        mAdapter.notifyDataSetChanged();
+                    }
+                });
+            }
+        });
+    }
+
+    private void sendSyncRequest() {
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                OkHttpClient okHttpClient = new OkHttpClient();
+                //创建网络请求
+                String url = "http://gank.io/api/data/福利/10/1";
+                Request request = new Request.Builder().get().url(url).build();
+
+                //同步
+                try {
+                    Response response = okHttpClient.newCall(request).execute();
+                    mgson = new Gson();
+                    String result = response.body().string();
+                    ResultBean resultBean = mgson.fromJson(result, ResultBean.class);
+
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }).start();
+
+    }
+
+    class GirlAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return mlist.size();
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder holder = null;
+            if (convertView == null) {
+                holder = new ViewHolder();
+                convertView = View.inflate(MainActivity.this, R.layout.item_girls_view, null);
+                holder.ivGirls = (ImageView) convertView.findViewById(R.id.iv_girls);
+                holder.tvTime = (TextView) convertView.findViewById(R.id.tv_time);
+
+                convertView.setTag(holder);
+
+            } else {
+                holder = (ViewHolder) convertView.getTag();
+            }
+
+            ResultBean.ResultsBean resultsBean = mlist.get(position);
+            holder.tvTime.setText(resultsBean.getPublishedAt());
+
+            /*Glide.with(MainActivity.this)
+                    .load(resultsBean.getUrl())
+                    .centerCrop()
+                    .into(holder.ivGirls);*/
+
+            Glide.with(MainActivity.this).load(resultsBean.getUrl()).centerCrop()
+                    .bitmapTransform(new CropCircleTransformation(MainActivity.this))
+                    .into(holder.ivGirls);
+
+            return convertView;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+    }
+
+    static class ViewHolder {
+        ImageView ivGirls;
+        TextView tvTime;
+    }
 
 }
---
+++ heimagirl/src/main/java/com/example/itheima/heimagirl/ResultBean.java
@@ -0,0 +1,127 @@
+package com.example.itheima.heimagirl;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2017-03-10.
+ */
+
+public class ResultBean {
+
+    public boolean error;
+    public List<ResultsBean> results ;
+
+    public boolean isError() {
+        return error;
+    }
+
+    public void setError(boolean error) {
+        this.error = error;
+    }
+
+    public List<ResultsBean> getResults() {
+        return results;
+    }
+
+    public void setResults(List<ResultsBean> results) {
+        this.results = results;
+    }
+
+    public class  ResultsBean{
+
+        /**
+         * _id : 58c1f808421aa95810795c34
+         * createdAt : 2017-03-10T08:49:12.756Z
+         * desc : 3-10
+         * publishedAt : 2017-03-10T11:43:50.30Z
+         * source : chrome
+         * type : 福利
+         * url : http://7xi8d6.com1.z0.glb.clouddn.com/2017-03-10-17127037_231706780569079_1119464847537340416_n.jpg
+         * used : true
+         * who : 代码家
+         */
+
+        private String _id;
+        private String createdAt;
+        private String desc;
+        private String publishedAt;
+        private String source;
+        private String type;
+        private String url;
+        private boolean used;
+        private String who;
+
+        public String get_id() {
+            return _id;
+        }
+
+        public void set_id(String _id) {
+            this._id = _id;
+        }
+
+        public String getCreatedAt() {
+            return createdAt;
+        }
+
+        public void setCreatedAt(String createdAt) {
+            this.createdAt = createdAt;
+        }
+
+        public String getDesc() {
+            return desc;
+        }
+
+        public void setDesc(String desc) {
+            this.desc = desc;
+        }
+
+        public String getPublishedAt() {
+            return publishedAt;
+        }
+
+        public void setPublishedAt(String publishedAt) {
+            this.publishedAt = publishedAt;
+        }
+
+        public String getSource() {
+            return source;
+        }
+
+        public void setSource(String source) {
+            this.source = source;
+        }
+
+        public String getType() {
+            return type;
+        }
+
+        public void setType(String type) {
+            this.type = type;
+        }
+
+        public String getUrl() {
+            return url;
+        }
+
+        public void setUrl(String url) {
+            this.url = url;
+        }
+
+        public boolean isUsed() {
+            return used;
+        }
+
+        public void setUsed(boolean used) {
+            this.used = used;
+        }
+
+        public String getWho() {
+            return who;
+        }
+
+        public void setWho(String who) {
+            this.who = who;
+        }
+    }
+
+}
